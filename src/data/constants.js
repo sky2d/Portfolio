cf8879a02771f3f0ef60648f6fa54870629e8f6a
sky2d <143280010+sky2d@users.noreply.github.com>
2023-10-25 20:05:12 +0530

    Resume Link changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sky2d",
   resume:
-    "https://drive.google.com/file/d/1ED-AVIC5O0e1yRwkfRU5MZt9r8nas0qb/view?usp=drive_link",
+    "https://drive.google.com/file/d/1kkzXVA4N4GC6TOEJETvS7bn1THs3minM/view",
   linkedin: "https://www.linkedin.com/in/akash-patel-8b02241aa/",
 };
 
